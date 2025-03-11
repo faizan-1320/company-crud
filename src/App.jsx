b9774bac05349a289145105e73b85353d9705a94
faizan-1320 <faizandiwan921@gmail.com>
2025-03-11 13:12:14 +0530

    add project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header/Header'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='mainheader'>Company Manangement System</h1>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navabar from '../Navabar/Navabar'
+
+export default function Header() {
+  return (
+    <>
+    <h2 className='topheader'>COMPANY CRUD OPERATIONS</h2>
+    <Navabar></Navabar>
+    </>
+  )
+}
